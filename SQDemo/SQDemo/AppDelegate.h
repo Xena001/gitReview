9fc75dd23f08cbaa83d7f87a882909d3c5bd2c61
“Xena001” <2668219044@qq.com>
2020-12-21 13:45:53 +0800

    feat:新建项目

---
+++ SQDemo/SQDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQDemo
+//
+//  Created by Mac on 2020/12/21.
+//  Copyright © 2020年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQDemo/SQDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQDemo
+//
+//  Created by Mac on 2020/12/21.
+//  Copyright © 2020年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
